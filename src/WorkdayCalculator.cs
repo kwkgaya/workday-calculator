c82fbab34c49b48219c65eb4fd9596e40db2b69c
Kavinda Gayashan <kavinda.gayashan@ifs.com>
2024-11-03 10:57:23 +0530

    Add the WorkdayCalculator class

---
+++ src/WorkdayCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workdayCalculator;
+
+public class WorkdayCalculator(TimeSpan workdayStart, TimeSpan workdayEnd)
+{
+    private readonly TimeSpan workdayStart = workdayStart;
+    private readonly TimeSpan workdayEnd = workdayEnd;
+    private readonly List<DateTime> Holidays = new List<DateTime>();
+    private readonly List<DateTime> RecurringHolidays = new List<DateTime>();
+
+    public void AddHoliday(DateTime date)
+    {
+        Holidays.Add(date.Date);
+    }
+
+    public void AddRecurringHoliday(DateTime date)
+    {
+        RecurringHolidays.Add(date.Date);
+    }
+
+    public DateTime AddWorkingDays(DateTime start, double workingDays)
+    {
+        return DateTime.MinValue;
+    }
+}
